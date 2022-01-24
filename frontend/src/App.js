dfb06e932dfc412f488b57b85b8233967862dd62
Justin Ly <jly3@ualberta.ca>
2022-01-23 17:49:24 -0700

    ran prettier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,68 +1,55 @@
-import React, {useEffect, useState} from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
-import "./App.css";
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
 
 function App() {
-
-  const [account, setAccount] = useState("");
+  const [account, setAccount] = useState('');
 
   const checkWalletConnection = async () => {
-    const {ethereum} = window;
+    const { ethereum } = window;
     if (!ethereum) {
-      console.log('Metamask not installed')
+      console.log('Metamask not installed');
       return;
-    }
-    else {
-      console.log('Ethereum object detected', ethereum)
+    } else {
+      console.log('Ethereum object detected', ethereum);
     }
 
-    const accounts = await ethereum.request({method: 'eth_accounts' });
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
 
     if (accounts.length !== 0) {
-      const account = accounts[0]
-      setAccount(account)
-  
+      const account = accounts[0];
+      setAccount(account);
     } else {
-      console.log("No account found")
+      console.log('No account found');
     }
-  }
+  };
 
   const connectWallet = async () => {
     try {
       const { ethereum } = window;
       if (!ethereum) {
-        alert("Metamask not installed, please install metamask!")
+        alert('Metamask not installed, please install metamask!');
         return;
       }
 
-      const accounts = await ethereum.request({ method: "eth_requestAccounts"});
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
 
       setAccount(accounts[0]);
-
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
-
+  };
 
   useEffect(() => {
     checkWalletConnection();
-  },[])
+  }, []);
 
-  
   return (
     <div className="App">
       <header className="App-header">
         <p>Hello Warld</p>
       </header>
-      {account !== "" ?
-      <p>Account connected</p> :
-      <button onClick={connectWallet}>Login</button>
-      }
+      {account !== '' ? <p>Account connected</p> : <button onClick={connectWallet}>Login</button>}
       <Router>
         <Routes>
           <Route path="/"></Route>
