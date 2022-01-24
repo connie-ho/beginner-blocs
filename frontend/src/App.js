8dd05cdf53995556bc1435b3cf82bda375b89b95
Justin Ly <jly3@ualberta.ca>
2022-01-23 17:10:33 -0700

    added connect wallet and check wallet connection functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {
   BrowserRouter as Router,
   Routes,
@@ -10,11 +10,62 @@ import {
 import "./App.css";
 
 function App() {
+
+  const [account, setAccount] = useState("");
+
+  const checkWalletConnection = async () => {
+    const {ethereum} = window;
+    if (!ethereum) {
+      console.log('Metamask not installed')
+      return;
+    }
+    else {
+      console.log('Ethereum object detected', ethereum)
+    }
+
+    const accounts = await ethereum.request({method: 'eth_accounts' });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0]
+      setAccount(account)
+  
+    } else {
+      console.log("No account found")
+    }
+  }
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        alert("Metamask not installed, please install metamask!")
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts"});
+
+      setAccount(accounts[0]);
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
+  useEffect(() => {
+    checkWalletConnection();
+  },[])
+
+  
   return (
     <div className="App">
       <header className="App-header">
         <p>Hello Warld</p>
       </header>
+      {account !== "" ?
+      <p>Account connected</p> :
+      <button onClick={connectWallet}>Connect Wallet</button>
+      }
       <Router>
         <Routes>
           <Route path="/"></Route>
