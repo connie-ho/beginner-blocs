3ec47492dacec6f0bbcf146239dbc419a8478caf
Justin Ly <jly3@ualberta.ca>
2022-01-25 19:15:50 -0700

    fixed to allow for false logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { ethers } from 'ethers';
 
 function App() {
   const [account, setAccount] = useState('');
@@ -14,13 +15,19 @@ function App() {
       console.log('Ethereum object detected', ethereum);
     }
 
-    const accounts = await ethereum.request({ method: 'eth_accounts' });
+    let loggedIn = localStorage.getItem('loggedIn') || null;
+    //check if user has already logged in, then set account state
+    if (loggedIn) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum, 'any');
+      const accounts = await provider.send('eth_accounts', []);
 
-    if (accounts.length !== 0) {
-      const account = accounts[0];
-      setAccount(account);
-    } else {
-      console.log('No account found');
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        setAccount(account);
+        return;
+      } else {
+        console.log('No account found');
+      }
     }
   };
 
@@ -31,25 +38,41 @@ function App() {
         alert('Metamask not installed, please install metamask!');
         return;
       }
+      const provider = new ethers.providers.Web3Provider(window.ethereum, 'any');
+      await provider.send('wallet_requestPermissions', [
+        {
+          eth_accounts: {},
+        },
+      ]);
+      const accounts = await provider.send('eth_requestAccounts');
 
-      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-
+      localStorage.setItem('loggedIn', 'true');
       setAccount(accounts[0]);
     } catch (error) {
       console.log(error);
     }
   };
 
+  const disconnectWallet = async () => {
+    localStorage.removeItem('loggedIn');
+    setAccount('');
+    return;
+  };
+
   useEffect(() => {
     checkWalletConnection();
-  }, []);
+  }, [account]);
 
   return (
     <div className="App">
       <header className="App-header">
         <p>Hello Warld</p>
       </header>
-      {account !== '' ? <p>Account connected</p> : <button onClick={connectWallet}>Login</button>}
+      {account !== '' ? (
+        <button onClick={disconnectWallet}>Logout</button>
+      ) : (
+        <button onClick={connectWallet}>Login</button>
+      )}
       <Router>
         <Routes>
           <Route path="/"></Route>
--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,2 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
+import user from '@testing-library/user-event';
+import { ethers } from 'ethers';
 import App from './App';
+
+describe('Application', () => {
+  it('renders main page appropriately', async () => {
+    render(<App />);
+    expect(await screen.queryByText('Hello Warld')).toBeInTheDocument();
+  });
+});
