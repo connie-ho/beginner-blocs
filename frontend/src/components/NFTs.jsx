e265328af176a15c4a69b24072242ee16d27bc0f
Justin Ly <jly3@ualberta.ca>
2022-02-20 13:06:09 -0500

    remove unused deps

--- frontend/src/components/NFTs.jsx
+++ frontend/src/components/NFTs.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Grid } from '@mui/material';
 
 import { useGetNFTs } from '../hooks/use-get-nfts';
