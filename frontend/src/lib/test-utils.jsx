dc116dbfd6d2ec207fb23aba45911105b25e40c9
Connie Ho <hconnie09@gmail.com>
2022-02-21 14:53:17 -0700

    empty line

--- frontend/src/lib/test-utils.jsx
+++ frontend/src/lib/test-utils.jsx
@@ -3,7 +3,6 @@ import { renderHook } from "@testing-library/react-hooks";
 
 import Providers from "../Providers";
 
-
 const customRender = (ui, options = {}) => render(ui, { wrapper: Providers, ...options });
 
 const customRenderHook = (callback) =>
