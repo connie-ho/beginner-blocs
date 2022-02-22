98a3b24f91f25e4e7132c5f97731516e0a783c3d
Justin Ly <jly3@ualberta.ca>
2022-02-22 12:09:31 -0500

    added NFTList, and Profile tests

---
+++ frontend/src/components/profile/__tests__/NFTList.test.jsx
@@ -0,0 +1,58 @@
+import { screen } from '@testing-library/react';
+import { renderWithProviders } from '../../../lib/test-utils';
+import { randomHexString } from '@ethersproject/testcases';
+import seedrandom from 'seedrandom';
+
+import NFTList from '../NFTList';
+
+const seed = new seedrandom(`BEGINNERBLOCS${Math.random()*100}`);
+
+const mockProps = {
+  items: [
+    {
+      address:randomHexString(seed, 0, 32),
+      description: "Test",
+      image: "",
+      name: "Test NFT",
+      owner: randomHexString(seed, 0, 32),
+      tokenId: Math.random()*100
+    },
+    {
+      address:randomHexString(seed, 0, 32),
+      description: "Test",
+      image: "",
+      name: "Test NFT",
+      owner: randomHexString(seed, 0, 32),
+      tokenId: Math.random()*100
+    },
+    
+  ],
+  type: 'owned'
+}
+
+describe('NFTList', () => {
+    const renderNFTList = async (props) => {
+        const view = renderWithProviders(<NFTList {...props} />)
+
+        return {
+            ...view,
+            $findNFTItems: async () => screen.findAllByTestId(/^nft-item-/),
+        }
+    } 
+
+    test('it renders', async() => {
+        const { container } = await renderNFTList(mockProps)
+        expect(container).toBeVisible()
+    })
+
+    test('should display a list of nfts items', async() => {
+        const { $findNFTItems } = await renderNFTList(mockProps);
+        const NFTItems = await $findNFTItems()
+        expect(NFTItems).toHaveLength(2)
+    })
+
+    test('should display a message if no items are found', async() => {
+      const { getByText } = await renderNFTList({items:[], type:'owned'});
+      expect(getByText("No Items Found!")).toBeInTheDocument();
+  })
+})
