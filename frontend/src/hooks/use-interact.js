1f41bbb861329825d331e099471e10ec25a0f34b
Akshay Rakheja <a3rakhej@uwaterloo.ca>
2022-02-23 02:07:53 -0500

    Adding minter, pinata, useinteract-> works locally and mints nfts

--- frontend/src/hooks/use-interact.js
+++ frontend/src/hooks/use-interact.js
@@ -1,96 +1,143 @@
-import { pinJSONToIPFS } from './use-pinata.js';
-import { useState, useEffect } from 'react';
-import useWalletConnection from './use-wallet-connection';
-import { nftAddress } from '../config';
-// import 'frontend/src/artifacts/contracts/NFT.sol/NFT.json' as contractABI; //confirm where abi is placed
-
+import { pinJSONToIPFS } from './use-pinata';
+import { nftaddress } from '../config';
 require('dotenv').config();
 const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
 const contractABI = require('../artifacts/contracts/NFT.sol/NFT.json');
-//const contractAddress = "0x4C4a07F737Bf57F6632B6CAB089B78f62385aCaE"; // need to change it to our contract address
+const contractAddress = nftaddress;
 const { createAlchemyWeb3 } = require('@alch/alchemy-web3');
 const web3 = createAlchemyWeb3(alchemyKey);
 
-function useInteract() {
-  const { account, connectWallet, disconnectWallet, addWalletListener } = useWalletConnection();
-
-  //default states
-  const [walletAddress, setWallet] = useState('');
-  //const [status, setStatus] = useState("");    -> dont need to set a status
-  const [name, setName] = useState('');
-  const [description, setDescription] = useState('');
-  const [url, setURL] = useState('');
-
-  const connectWalletPressed = async () => {
-    const walletResponse = await connectWallet();
-    //setStatus(walletResponse.status);
-    setWallet(walletResponse);
-  };
-
-  const onMintPressed = async () => {
-    const { success, status } = await mintNFT(url, name, description);
-    //setStatus(status);
-    if (success) {
-      setName('');
-      setDescription('');
-      setURL('');
-    }
-  };
-
-  const mintNFT = async (url, name, description) => {
-    if (url.trim() === '' || name.trim() === '' || description.trim() === '') {
-      return {
-        success: false,
-        status: '❗Please make sure all fields are completed before minting.',
-      };
-    }
-
-    //make metadata
-    const metadata = new Object();
-    metadata.name = name;
-    metadata.image = url;
-    metadata.description = description;
-
-    const pinataResponse = await pinJSONToIPFS(metadata);
-    if (!pinataResponse.success) {
-      return {
-        success: false,
-        status: '😢 Something went wrong while uploading your tokenURI.',
-      };
-    }
-    const tokenURI = pinataResponse.pinataUrl;
-
-    window.contract = await new web3.eth.Contract(contractABI, nftAddress);
-
-    const transactionParameters = {
-      to: nftAddress, // Required except during contract publications.
-      from: window.ethereum.selectedAddress, // must match user's active address.
-      data: window.contract.methods.mintNFT(window.ethereum.selectedAddress, tokenURI).encodeABI(),
-    };
-
+export const connectWallet = async () => {
+  if (window.ethereum) {
     try {
-      const txHash = await window.ethereum.request({
-        method: 'eth_sendTransaction',
-        params: [transactionParameters],
+      const addressArray = await window.ethereum.request({
+        method: 'eth_requestAccounts',
       });
-      return {
-        success: true,
-        status: '✅ Check out your transaction on Etherscan: https://ropsten.etherscan.io/tx/' + txHash,
+      const obj = {
+        status: '👆🏽 Write a message in the text-field above.',
+        address: addressArray[0],
       };
-    } catch (error) {
+      return obj;
+    } catch (err) {
       return {
-        success: false,
-        status: '😥 Something went wrong: ' + error.message,
+        address: '',
+        status: '😥 ' + err.message,
       };
     }
+  } else {
+    return {
+      address: '',
+      status: (
+        <span>
+          <p>
+            {' '}
+            🦊{' '}
+            <a target="_blank" href={`https://metamask.io/download.html`} rel="noreferrer">
+              You must install Metamask, a virtual Ethereum wallet, in your browser.
+            </a>
+          </p>
+        </span>
+      ),
+    };
+  }
+};
+
+export const getCurrentWalletConnected = async () => {
+  if (window.ethereum) {
+    try {
+      const addressArray = await window.ethereum.request({
+        method: 'eth_accounts',
+      });
+      if (addressArray.length > 0) {
+        return {
+          address: addressArray[0],
+          status: '👆🏽 Write a message in the text-field above.',
+        };
+      } else {
+        return {
+          address: '',
+          status: '🦊 Connect to Metamask using the top right button.',
+        };
+      }
+    } catch (err) {
+      return {
+        address: '',
+        status: '😥 ' + err.message,
+      };
+    }
+  } else {
+    return {
+      address: '',
+      status: (
+        <span>
+          <p>
+            {' '}
+            🦊{' '}
+            <a target="_blank" href={`https://metamask.io/download.html`} rel="noreferrer">
+              You must install Metamask, a virtual Ethereum wallet, in your browser.
+            </a>
+          </p>
+        </span>
+      ),
+    };
+  }
+};
+
+// async function loadContract() {
+//   return new web3.eth.Contract(contractABI, contractAddress);
+// }
+
+export const mintNFT = async (file, name, description) => {
+  if (file === '' || name.trim() === '' || description.trim() === '') {
+    return {
+      success: false,
+      status: '❗Please make sure all fields are completed before minting.',
+    };
+  }
+
+  // make metadata
+  const metadata = new Object();
+  metadata.name = name;
+  metadata.image = file.image;
+  metadata.description = description;
+
+  console.log('here is image metadata:', metadata.image);
+  const pinataResponse = await pinJSONToIPFS(metadata);
+  if (!pinataResponse.success) {
+    return {
+      success: false,
+      status: '😢 Something went wrong while uploading your tokenURI.',
+    };
+  }
+  const tokenURI = pinataResponse.pinataUrl;
+  console.log('tokenuri is :' + tokenURI);
+
+  console.log('this is contract addresss: ', contractAddress);
+  console.log('this is contract addresss: ', contractABI);
+
+  window.contract = await new web3.eth.Contract(contractABI.abi, contractAddress);
+
+  console.log('this is window.contract: ', window.contract);
+
+  const transactionParameters = {
+    to: contractAddress, // Required except during contract publications.
+    from: window.ethereum.selectedAddress, // must match user's active address.
+    data: window.contract.methods.createToken(tokenURI).encodeABI(),
   };
 
-  return {
-    walletAddress,
-    mintNFT,
-    onMintPressed,
-    connectWalletPressed,
-  };
-}
-
-export default useInteract;
+  try {
+    const txHash = await window.ethereum.request({
+      method: 'eth_sendTransaction',
+      params: [transactionParameters],
+    });
+    return {
+      success: true,
+      status: '✅ Check out your transaction on Etherscan: https://ropsten.etherscan.io/tx/' + txHash,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      status: '😥 Something went wrong: ' + error.message,
+    };
+  }
+};
