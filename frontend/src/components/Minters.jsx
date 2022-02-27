6389561f8bfca0afe33487b244ed65fb0b217ada
Akshay Rakheja <a3rakhej@uwaterloo.ca>
2022-02-27 16:40:30 -0500

    Co-authored-by: Justin Ly <Justin1002@users.noreply.github.com> Co-authored-by: Connie Ho <connie-ho@users.noreply.github.com>

--- frontend/src/components/Minters.jsx
+++ frontend/src/components/Minters.jsx
@@ -122,39 +122,39 @@ const Minter = (props) => {
         width: 1728,
         height: 830,
         align: "center",
-        backgroundColor: 'primary.light',
+        backgroundColor: 'white',
       }}>
           <br></br>
 
     <Grid container spacing={2}>
-        <Grid item xs={12} sx={{ color: 'text.light' }}>
+        <Grid item xs={12} sx={{ color: 'text.dark' }}>
           <Typography className="MuiTypography-alignJustify" variant ="h3" align ="center">Create Your Own NFT!</Typography>
         </Grid>
-        <Grid item xs={12}sx={{ color: 'text.light'}}>
+        <Grid item xs={12}sx={{ color: 'text.dark'}}>
             <Typography paragraph = "true" className="MuiTypography-subtitle1" variant ="subtitle1" align ="center" variant ="h4"> Simply upload your Asset's Image, add a Name, and a Description for your Asset<br/>Then press "Mint NFT"
       </Typography>
         </Grid>
         <Grid item xs={2} ></Grid>
         <Grid item xs={6}>
             
-            <Grid item xs={12} sx={{  color: 'text.light', mt: 5}}>
-                <Typography className="MuiTypography-subtitle1" variant ="subtitle1" align ="left" variant ="h4">🖼 Upload Image:   <Input disableUnderline="true" type="file" sx ={{ml: 5,pb:2, color: "white"}}className="MuiInput-formControl"   onChange={onChange}/></Typography>
+            <Grid item xs={12} sx={{  color: 'text.dark', mt: 5}}>
+                <Typography className="MuiTypography-subtitle1" variant ="subtitle1" align ="left" variant ="h4">🖼 Upload Image:   <Input disableUnderline="true" type="file" sx ={{ml: 5,pb:2, color: "black"}}className="MuiInput-formControl"   onChange={onChange}/></Typography>
             </Grid>
-             <Grid item xs={12} sx={{  color: 'text.light', mt: 5}}>
+             <Grid item xs={12} sx={{  color: 'text.dark', mt: 5}}>
                 <Typography className="MuiTypography-subtitle1" variant ="subtitle1" align ="left" variant ="h4"> Name: 
-                  <Input sx ={{ml:5, pl:5,color: "white"}}className="MuiInput-formControl"  placeholder="e.g. My first NFT!"  onChange={(event) => setName(event.target.value)}/>
+                  <Input sx ={{ml:5, pl:5,color: "black"}}className="MuiInput-formControl"  placeholder="e.g. My first NFT!"  onChange={(event) => setName(event.target.value)}/>
                 </Typography>
             </Grid>
             <Grid item xs={12}><br></br></Grid>
-            <Grid item xs={12} sx={{ color: 'text.light', mt: 5}}>
-               <Typography className="MuiTypography-subtitle1" variant ="subtitle1" align ="left" variant ="h4">Description of your NFT: <Input sx ={{ml:2, pl:2 ,color: "white", width: 300}}className="MuiInput-formControl"  placeholder="e.g. Even cooler than cryptokitties" onChange={(event) => setDescription(event.target.value)}/></Typography>
+            <Grid item xs={12} sx={{ color: 'text.dark', mt: 5}}>
+               <Typography className="MuiTypography-subtitle1" variant ="subtitle1" align ="left" variant ="h4">Description of your NFT: <Input sx ={{ml:2, pl:2 ,color: "black", width: 300}}className="MuiInput-formControl"  placeholder="e.g. Even cooler than cryptokitties" onChange={(event) => setDescription(event.target.value)}/></Typography>
             </Grid>
 
 
         </Grid>
        
-        <Grid item xs={2} style={{padding: "0", margin:"0"}}  sx={{ align:"center" , border:'1px solid grey', height:"400px", borderRadius:"10px"}}>
-        <img style={{ border:'1px solid grey', borderRadius:"10px"}} width="100%" src = {file.image} alt="Preview"/>
+        <Grid item xs={2} style={{padding: "0", margin:"0"}}  sx={{ align:"center" , border:'1px solid grey', borderRadius:"10px", minHeight:"20rem", minWidth:"20rem"}}>
+        <img style={{ border:'1px solid grey', borderRadius:"10px", objectFit:"cover", height:"100%" }} width="100%" src = {file.image} alt="Preview"/>
         </Grid>
         </Grid>
     
@@ -165,7 +165,7 @@ const Minter = (props) => {
         </Box>
         <br/><br/><br/>
        
-        <Typography className="MuiTypography-paragraph" variant ="subtitle1" align ="center" paragraph = "true" color={"white"}> <Link href={status.slice(-98)} rel="noopener noreferrer" target="_blank">{status}</Link> </Typography>
+        <Typography className="MuiTypography-paragraph" variant ="subtitle1" align ="center" paragraph = "true" color={"black"}> <Link href={status.slice(-98)} rel="noopener noreferrer" target="_blank">{status}</Link> </Typography>
        
         </Box>
     </div>
