5b09a64396a534f5134af12597fdc77c7e4175d4
Akshay Rakheja <a3rakhej@uwaterloo.ca>
2022-03-14 18:40:41 -0400

    Adding test for minters

---
+++ packages/client/src/components/__tests__/Minters.test.jsx
@@ -0,0 +1,31 @@
+// import userEvent from '@testing-library/user-event';
+// import { screen } from "@testing-library/react"
+
+// import { renderWithProviders } from "../../lib/test-utils"
+
+// import Minter from '../Minters';
+
+describe('Minters', () => {
+  // const renderCreate = async(props) => {
+  //     const view = renderWithProviders(<Minter {...props}/>)
+  //     return {
+  //         ...view,
+  //         getStartedButton: screen.queryByRole('button', {name: /get started/i}),
+  //         $findMarketList: async () => screen.findByTestId('market-list')
+  //     }
+  // }
+  // test('it renders', async () => {
+  //     const {container} = await renderHome()
+  //     expect(container).toBeVisible()
+  // })
+  // test('should have all the appropriate components', async () => {
+  //     const { getStartedButton, $findMarketList } = await renderHome()
+  //     expect(await $findMarketList()).toBeVisible()
+  //     expect(getStartedButton).toBeVisible()
+  // })
+  // test('should redirect the user when the get started button is clicked', async () => {
+  //     const { getStartedButton } = await renderHome()
+  //     userEvent.click(getStartedButton)
+  //     expect(getStartedButton).toHaveAttribute('href', '/get-started');
+  // })
+});
