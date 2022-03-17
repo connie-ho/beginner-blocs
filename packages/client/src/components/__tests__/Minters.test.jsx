82061d687dbbef8d8d4dbb16832f2a62215b8617
Akshay Rakheja <a3rakhej@uwaterloo.ca>
2022-03-16 21:06:12 -0400

    Minters test and cleanup

--- packages/client/src/components/__tests__/Minters.test.jsx
+++ packages/client/src/components/__tests__/Minters.test.jsx
@@ -2,29 +2,50 @@
 import { screen } from '@testing-library/react';
 
 import Minter from '../Minters';
-
+import userEvent from '@testing-library/user-event';
 import { renderWithProviders } from '../../lib/test-utils';
+import { expect } from 'chai';
 
 describe('Minters', () => {
   const renderMinters = async (props) => {
     const view = renderWithProviders(<Minter {...props} />);
+    const fakeFile = new File(['hello'], 'hello.png', { type: 'image/png' });
 
     return {
       ...view,
+      fakeFile,
       // getStartedButton: screen.queryByRole('button', {name: /get started/i}),
-      chooseFileButton: screen.queryByRole('button', { name: /Choose File/i }),
-      mintNFTButton: screen.queryByRole('button', { name: /Mint NFT/i }),
+      mintNFTButton: screen.getByRole('button', { name: /mint nft/i }),
+      nftName: screen.queryByTestId('nameInput'),
     };
   };
+
   test('it renders', async () => {
     const { container } = await renderMinters();
     expect(container).toBeVisible();
   });
-  // test('should have all the appropriate components', async () => {
-  //     const { getStartedButton, $findMarketList } = await renderHome()
-  //     expect(await $findMarketList()).toBeVisible()
-  //     expect(getStartedButton).toBeVisible()
+  test('User File Upload', async () => {
+    const { fakeFile } = renderMinters();
+    jest.spyOn(global, 'FileReader').mockImplementation(function () {
+      this.readAsDataURL = jest.fn();
+    });
+    // const inputFile = screen.getByTestId(/imageUpload/i);
+    const imageField = screen.queryByTestId(/imageUpload/i);
+    // console.log(inputFile);
+    userEvent.upload(imageField, fakeFile);
+    expect(imageField.files).toHaveLength(1);
+  });
+  // test('Check Name field', async () => {
+  //     // const { nftName } = await renderMinters();
+  //     userEvent.type(nftName,":Test NFT:");
+  //     expect(nftName).to.not.be.null;
+  //     // expect(await $findMarketList()).toBeVisible()
+  //     // expect(getStartedButton).toBeVisible()
   // })
+
+  // function clickMintButton() {
+  //     user.click(screen.getByRole('button', { name: /mint nft/i }));
+  // }
   // test('should redirect the user when the get started button is clicked', async () => {
   //     const { getStartedButton } = await renderHome()
   //     userEvent.click(getStartedButton)
--- packages/server/src/app.js
+++ packages/server/src/app.js
@@ -5,13 +5,15 @@ import logger from 'morgan';
 import cors from 'cors';
 import indexRouter from './routes/index/index';
 import nftMetaDataRouter from './routes/nft-meta-data/nft-meta-data';
+import bodyParser from 'body-parser';
 
 const app = express();
 
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.bodyParser());
+app.use(bodyParser.json());
+// app.use(express.bodyParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
