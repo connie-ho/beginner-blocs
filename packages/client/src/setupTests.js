038bf8e4db1a9d48dce5aaf6b523de4c42f7b08a
Sufiyan <sufigani@gmail.com>
2022-03-23 13:00:34 -0400

    added tests for  success and notfound component

--- packages/client/src/setupTests.js
+++ packages/client/src/setupTests.js
@@ -4,9 +4,9 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/extend-expect';
-import { generateTestingUtils } from 'eth-testing';
+import { setupEthTesting } from 'eth-testing';
 
-const { provider } = generateTestingUtils({ providerType: 'MetaMask' });
+const { provider } = setupEthTesting({ providerType: 'MetaMask' });
 
 global.beforeAll(() => {
   global.window.ethereum = provider;
