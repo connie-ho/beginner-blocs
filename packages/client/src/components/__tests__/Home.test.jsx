10997f923ee7e455e5d31b8c8f9dc2b5e40d462a
Yulin Wan <yulinwan@Yulins-Air.lan>
2022-03-24 18:54:42 -0400

    fix lint

--- packages/client/src/components/__tests__/Home.test.jsx
+++ packages/client/src/components/__tests__/Home.test.jsx
@@ -1,38 +1,37 @@
 import userEvent from '@testing-library/user-event';
-import { screen } from "@testing-library/react"
+import { screen } from '@testing-library/react';
 
-import { renderWithProviders } from "../../lib/test-utils"
+import { renderWithProviders } from '../../lib/test-utils';
 
-import Home from '../Home'
+import Home from '../Home';
 
 describe('Home', () => {
-    const renderHome = async(props) => {
-        const view = renderWithProviders(<Home {...props}/>)
+  const renderHome = async (props) => {
+    const view = renderWithProviders(<Home {...props} />);
 
-        return {
-            ...view,
-            getStartedButton: screen.queryByRole('button', {name: /get started/i}),
-            $findMarketList: async () => screen.findByTestId('market-list')
-        }
-    }
+    return {
+      ...view,
+      getStartedButton: screen.queryByRole('button', { name: /get started/i }),
+      $findMarketList: async () => screen.findByTestId('market-list'),
+    };
+  };
 
-    test('it renders', async () => {
-        const {container} = await renderHome()
-        expect(container).toBeVisible()
-    })
+  test('it renders', async () => {
+    const { container } = await renderHome();
+    expect(container).toBeVisible();
+  });
 
-    test('should have all the appropriate components', async () => {
-        const { getStartedButton, $findMarketList } = await renderHome()
+  test('should have all the appropriate components', async () => {
+    const { getStartedButton, $findMarketList } = await renderHome();
 
-        expect(await $findMarketList()).toBeVisible()
-        expect(getStartedButton).toBeVisible()
-    })
+    expect(await $findMarketList()).toBeVisible();
+    expect(getStartedButton).toBeVisible();
+  });
 
-    test('should redirect the user when the get started button is clicked', async () => {
-        const { getStartedButton } = await renderHome()
-        
-        userEvent.click(getStartedButton)
-        expect(getStartedButton).toHaveAttribute('href', '/get-started');
-    })
+  test('should redirect the user when the get started button is clicked', async () => {
+    const { getStartedButton } = await renderHome();
 
-})
+    userEvent.click(getStartedButton);
+    expect(getStartedButton).toHaveAttribute('href', '/get-started');
+  });
+});
