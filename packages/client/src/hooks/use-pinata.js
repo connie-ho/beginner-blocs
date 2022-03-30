62ea0a0e560d380f4e6a385c09aac50d497c84e7
Akshay Rakheja <a3rakhej@uwaterloo.ca>
2022-03-30 05:48:56 -0400

    refactored use-pinata

--- packages/client/src/hooks/use-pinata.js
+++ packages/client/src/hooks/use-pinata.js
@@ -1,31 +1,18 @@
-require('dotenv').config();
-const key = process.env.REACT_APP_PINATA_KEY;
-const secret = process.env.REACT_APP_PINATA_SECRET;
+// require('dotenv').config();
+// const key = process.env.REACT_APP_PINATA_KEY;
+// const secret = process.env.REACT_APP_PINATA_SECRET;
 const axios = require('axios');
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const pinJSONToIPFS = async (JSONbody) => {
   const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
 
-  // const buffer = Buffer.from(file, "base64");
-  // const stream = Readable.from(buffer);
-  // const filename = file.name;
-  // stream.path = filename;
-
-  // let data = new FormData();
-  // data.append('file', stream);
-
-  // const metadata = JSON.stringify({
-  //     name: name,
-  //     image: stream,
-  //     description: description
-  // });
-  // data.append('pinataMetadata', metadata);
-
   return axios
     .post(url, JSONbody, {
       headers: {
-        pinata_api_key: key,
-        pinata_secret_api_key: secret,
+        pinata_api_key: process.env.REACT_APP_PINATA_KEY,
+        pinata_secret_api_key: process.env.REACT_APP_PINATA_SECRET,
       },
     })
     .then(function (response) {
